354ff87dcc95eef145deda0d2bfb113a27fcb55c
Galiant <antonijodev@gmail.com>
2017-09-28 02:38:16 +0100

    Mobile first

--- bin/contact_me.php
+++ bin/contact_me.php
@@ -1,28 +1,26 @@
 <?php
 // check if fields passed are empty
-if(empty($_POST['name'])  		||
-   empty($_POST['email']) 		||
-   empty($_POST['subject']) 		||
-   empty($_POST['message'])	||
-   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
-   {
-	echo "No arguments Provided!";
-	return false;
-   }
-	
+if (empty($_POST['name'])        ||
+   empty($_POST['email'])       ||
+   empty($_POST['subject'])         ||
+   empty($_POST['message'])     ||
+   !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+    echo "No arguments Provided!";
+    return false;
+}
+    
 $name = $_POST['name'];
 $email_address = $_POST['email'];
 $email_subject = $_POST['subject'];
 $message = $_POST['message'];
-	
+    
 // create email body and send it	
-$to = 'info@apartmanitamaramiletic.com'; // put your email
+$to = 'tamaram1@net.hr'; // put your email
 $email_subject = "$email_subject:  $name";
 $email_body = "You have received a new message. \n\n".
-				  " Here are the details:\n \nName: $name \n ".
-				  "Email: $email_address\n Subject: $email_subject Message \n $message";
+                  " Here are the details:\n \nName: $name \n ".
+                  "Email: $email_address\n Subject: $email_subject Message \n $message";
 $headers = "From: $email_address\n";
-$headers .= "Reply-To: $email_address";	
-mail($to,$email_subject,$email_body,$headers);
-return true;			
-?>
+$headers .= "Reply-To: $email_address";
+mail($to, $email_subject, $email_body, $headers);
+return true;
